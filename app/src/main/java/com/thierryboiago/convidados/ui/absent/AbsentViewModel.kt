43e613a92366e15c563cd0d2c69657ef7d826080
Thierry Boiago <58778448+thierryboiago@users.noreply.github.com>
2023-12-07 11:58:21 -0300

    change values

--- app/src/main/java/com/thierryboiago/convidados/ui/absent/AbsentViewModel.kt
+++ app/src/main/java/com/thierryboiago/convidados/ui/absent/AbsentViewModel.kt
@@ -1,10 +1,10 @@
-package com.thierryboiago.convidados.ui.slideshow
+package com.thierryboiago.convidados.ui.absent
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AbsentViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
